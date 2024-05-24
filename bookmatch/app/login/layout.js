fc42000cc0d414ff53daf7ece17f6ee25881030c
diegoanro22 <diegorosalesv22@gmail.com>
2024-05-23 22:14:01 -0600

    arreglos

--- bookmatch/app/login/layout.js
+++ bookmatch/app/login/layout.js
@@ -5,19 +5,19 @@ import '../../css/globals.css';
 
 export default function LoginLayout({ children }) {
 return (
-<html lang="en" class="h-full bg-white">
+<html lang="en" className="h-full bg-white">
     <head>
     <meta charSet="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>BookMatch</title>
     </head>
     <body className={inter.className} class="h-full">
-    <div class="bg-white">
-    <header class="absolute inset-x-0 top-0 z-50">
-    <nav class="flex items-center justify-between p-6 lg:px-8 " aria-label="Global" >
-        <div class="flex lg:flex-1">
-            <span class="sr-only">hola</span>
-            <Image class="h-11 w-auto" 
+    <div className="bg-white">
+    <header className="absolute inset-x-0 top-0 z-50">
+    <nav className="flex items-center justify-between p-6 lg:px-8 " aria-label="Global" >
+        <div className="flex lg:flex-1">
+            <span className="sr-only">hola</span>
+            <Image className="h-11 w-auto" 
             src="/bookmatch.png" 
             alt=""
             width={500}
