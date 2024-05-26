429c51d1e4fe801c64d1f10d0a58fceb707f0764
erickguerra2 <eagi578@gmail.com>
2024-05-26 17:30:32 -0600

    Aregglo cards

--- bookmatch/app/main/page.js
+++ bookmatch/app/main/page.js
@@ -24,29 +24,36 @@ function MainPage() {
     setValue(newValue);
     };
 
+    const handleLogout = () => {
+        // logica para cerrar sesion
+        console.log("Cerrar sesión");
+    };
+
     return (
-    <div>
-        <Box sx={{ width: '100%' }}>
-        <AppBar position="static" color="primary" sx={{ backgroundColor: 'white' }} >
-            <Toolbar>
-        </Toolbar>
-        <Tabs value={value} onChange={handleChange} centered>
-          <Tab label="Libros" />
-          <Tab label="Géneros" />
-          <Tab label="Recomendaciones" />
-          <Tab label="Libros leidos"/>
-        </Tabs>
-      </AppBar>
-      {/* <Box sx={{ p: 3 }}> */}
-        {value === 0 && <TabPanel value={value} index={0}>
-  <LibrosPage />
-</TabPanel>}
-        {value === 1 && <TabPanel><GenerosPage /></TabPanel>}
-        {value === 2 && <TabPanel>Contenido de Recomendaciones</TabPanel>}
-        {value === 3 && <TabPanel><LibroLeidossPage/></TabPanel>}
-      </Box>
-    </div>
-  );
+        <div>
+            <Box sx={{ width: '100%' }}>
+                <AppBar position="static" color="primary" sx={{ backgroundColor: 'white' }} >
+                    <Toolbar>
+                    <Button color="inherit" onClick={handleLogout}>
+                            Cerrar sesión
+                        </Button>
+                    </Toolbar>
+                    <Tabs value={value} onChange={handleChange} centered>
+                        <Tab label="Libros" />
+                        <Tab label="Géneros" />
+                        <Tab label="Recomendaciones" />
+                        <Tab label="Libros leidos"/>
+                    </Tabs>
+                </AppBar>
+                {value === 0 && <TabPanel value={value} index={0}>
+                    <LibrosPage />
+                </TabPanel>}
+                {value === 1 && <TabPanel value={value} index={1}><GenerosPage /></TabPanel>}
+                {value === 2 && <TabPanel value={value} index={2}> Contenido de Recomendaciones</TabPanel>}
+                {value === 3 && <TabPanel value={value} index={3}><LibroLeidossPage/></TabPanel>}
+            </Box>
+        </div>
+    );
 }
 
 function TabPanel(props) {
@@ -75,18 +82,23 @@ function LibrosPage() {
     const libros = [
         { author: 'Autor 1', title: 'Libro 1', genre: 'Género 1' },
         { author: 'Autor 2', title: 'Libro 2', genre: 'Género 2' },
+        { author: 'Autor 3', title: 'Libro 3', genre: 'Género 2' },
+        { author: 'Autor 4', title: 'Libro 4', genre: 'Género 3' },
+        { author: 'Autor 5', title: 'Libro 5', genre: 'Género 3' },
+        { author: 'Autor 6', title: 'Libro 6', genre: 'Género 1' },
+        { author: 'Autor 7', title: 'Libro 7', genre: 'Género 5' },
         // mas libros
     ];
 
     return (
         <TabPanel>
             <h1>Libros</h1>
-            <Grid container spacing={2}>
+            <Grid container spacing={3}>
                 {(() => {
                     const cards = [];
                     for (let i = 0; i < libros.length; i++) {
                         cards.push(
-                            <Grid item xs={6} sm={6} md={9} lg={50} key={i}>
+                            <Grid item xs={6} sm={6} md={4} lg={4} key={i}>
                                 <Paper elevation={5}>
                                     <Grid container direction="column" alignItems="center">
                                         <Grid item xs={12}>
@@ -151,7 +163,11 @@ function LibrosPage() {
             const librosByGenre = [
                 { author: 'Autor 1', title: 'Libro 1', genre: 'Género 1' },
                 { author: 'Autor 2', title: 'Libro 2', genre: 'Género 2' },
-                // Agregue más libros aquí
+                { author: 'Autor 3', title: 'Libro 3', genre: 'Género 2' },
+                { author: 'Autor 4', title: 'Libro 4', genre: 'Género 3' },
+                { author: 'Autor 5', title: 'Libro 5', genre: 'Género 3' },
+                { author: 'Autor 6', title: 'Libro 6', genre: 'Género 1' },
+                { author: 'Autor 7', title: 'Libro 7', genre: 'Género 4' },
             ];
             setLibros(librosByGenre.filter((libro) => libro.genre === genre));
         }, [genre]);
@@ -159,17 +175,19 @@ function LibrosPage() {
         return (
             <TabPanel>
                 <h1>Géneros</h1>
-                <Grid container spacing={2}>
+                <Grid container spacing={3}>
                     <Grid item xs={12}>
                         <Typography variant="h5">Seleccione un género:</Typography>
                         <Select value={genre} onChange={handleGenreChange}>
                             <MenuItem value="Género 1">Género 1</MenuItem>
                             <MenuItem value="Género 2">Género 2</MenuItem>
+                            <MenuItem value="Género 3">Género 3</MenuItem>
+                            <MenuItem value="Género 4">Género 4</MenuItem>
                             {/* Agregue más opciones aquí */}
                         </Select>
                     </Grid>
                     {libros.map((libro, index) => (
-                        <Grid item xs={6} sm={6} md={9} lg={50} key={index}>
+                        <Grid item xs={6} sm={6} md={4} lg={4} key={index}>
                             <Paper elevation={5}>
                                 <Grid container direction="column" alignItems="center">
                                     <Grid item xs={12}>
@@ -227,7 +245,13 @@ function LibroLeidossPage() {
             const libros = [
                 { author: 'Autor 1', title: 'Libro 1', genre: 'Género 1', isRead: false },
                 { author: 'Autor 2', title: 'Libro 2', genre: 'Género 2', isRead: false },
-                // Agregue más libros aquí
+                { author: 'Autor 1', title: 'Libro 1', genre: 'Género 1', isRead: false },
+                { author: 'Autor 2', title: 'Libro 2', genre: 'Género 2', isRead: false },
+                { author: 'Autor 3', title: 'Libro 3', genre: 'Género 2', isRead: false },
+                { author: 'Autor 4', title: 'Libro 4', genre: 'Género 3', isRead: false },
+                { author: 'Autor 5', title: 'Libro 5', genre: 'Género 3', isRead: false },
+                { author: 'Autor 6', title: 'Libro 6', genre: 'Género 1', isRead: false },
+                { author: 'Autor 7', title: 'Libro 7', genre: 'Género 5', isRead: false },
             ];
             setLibros(libros);
         }, []);
@@ -235,9 +259,9 @@ function LibroLeidossPage() {
         return (
             <TabPanel>
                 <h1>Libros</h1>
-                <Grid container spacing={2}>
+                <Grid container spacing={3}>
                     {libros.map((libro, index) => (
-                        <Grid item xs={6} sm={6} md={9} lg={50} key={index}>
+                        <Grid item xs={6} sm={6} md={4} lg={4} key={index}>
                             <Paper elevation={5}>
                                 <Grid container direction="column" alignItems="center">
                                     <Grid item xs={12}>
