5a04c454afeb8fb6923de3b3c83dac618b712e2f
erickguerra2 <eagi578@gmail.com>
2024-05-27 20:59:54 -0600

    Mostrar generos

--- bookmatch/app/main/generos/page.js
+++ bookmatch/app/main/generos/page.js
@@ -1,58 +1,59 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TabPanel from '../../../components/TabPanel';
-import LibroCard from '../../../components/LibroCard';
 import Layout from '../layout';
 import Grid from '@mui/material/Grid';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import { obtenerGeneros } from '../../../lib/functions';
 
 const LibrosPage = () => {
     const [isRead, setIsRead] = useState(false);
-    const [value, setValue] = useState(1);
-    const [selectedGenre, setSelectedGenre] = useState(null);
+    const [value, setValue] = useState(0);
+    const [libros, setLibros] = useState([]);
+    const [generos, setGeneros] = useState([]);
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
 
-    const handleGenreChange = (event) => {
-        setSelectedGenre(event.target.value);
+  useEffect(() => {
+    const fetchGeneros = async () => {
+      try {
+        const generosObtenidos = await obtenerGeneros();
+        console.log('Géneros obtenidos:', generosObtenidos);
+        setGeneros(generosObtenidos);
+      } catch (error) {
+        console.error('Error al obtener los géneros:', error);
+      }
     };
+    fetchGeneros();
+  }, []);
 
-    const libros = [
-        { author: 'Autor 1', title: 'Libro 1', genre: 'Género 1', description: 'Descripción 1' },
-        { author: 'Autor 2', title: 'Libro 2', genre: 'Género 2', description: 'Descripción 2' },
-        { author: 'Autor 3', title: 'Libro 3', genre: 'Género 3', description: 'Descripción 3' },
-        { author: 'Autor 4', title: 'Libro 4', genre: 'Género 2', description: 'Descripción 4' },
-        { author: 'Autor 5', title: 'Libro 5', genre: 'Género 1', description: 'Descripción 5' },
-        { author: 'Autor 6', title: 'Libro 6', genre: 'Género 2', description: 'Descripción 6' },
-        // Más libros
-    ];
+  const handleReadClick = (index) => {
+    const newGenero = [...generos];
+    newGenero[index].isRead =!newGenero[index].isRead;
+    setGeneros(newGenero);
+  };
 
-    const handleReadClick = () => {
-        setIsRead(!isRead);
-    };
+  const allGeneros = generos.map(genero => genero.nombre).join(', ');
 
-    const filteredLibros = libros.filter((libro) => libro.genre === selectedGenre);
-
-    return (
-        <Layout value={value} handleChange={handleChange}>
-            <TabPanel>
-                <h1>Libros</h1>
-                <select value={selectedGenre} onChange={handleGenreChange} color="black" style={{ backgroundColor: 'transparent' }}>
-                    <option value="">Seleccione un género</option>
-                    <option value="Género 1">Género 1</option>
-                    <option value="Género 2">Género 2</option>
-                    <option value="Género 3">Género 3</option>
-                    {/* Agrega más opciones para cada género */}
-                </select>
-                <Grid container spacing={2}>
-                {filteredLibros.map((libro, index) => (
-                        <LibroCard key={index} libro={libro} handleReadClick={handleReadClick} isRead={isRead} />
-                    ))}
-                </Grid>
-            </TabPanel>
-        </Layout>
-    );
+  return (
+    <Layout value={value} handleChange={handleChange}>
+      <TabPanel>
+        <h1>Selecione los géneros</h1>
+        <List>
+          {generos.map((genero, index) => (
+            <ListItem key={index} button onClick={() => handleReadClick(index)}>
+              <ListItemText primary={genero.nombre} />
+            </ListItem>
+          ))}
+        </List>
+        <h2>Todos los géneros: {allGeneros}</h2>
+      </TabPanel>
+    </Layout>
+  );
 };
 
 export default LibrosPage;
