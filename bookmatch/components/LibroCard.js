db86d9d2dc2eb93a0ad8081ccbf735623a823eef
GenserDev <genserdev@gmail.com>
2024-05-27 20:08:15 -0600

    Se modificaron las Cards

--- bookmatch/components/LibroCard.js
+++ bookmatch/components/LibroCard.js
@@ -2,38 +2,38 @@ import React from 'react';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ThumbDownIcon from '@mui/icons-material/ThumbDown';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 import { useTheme } from '@mui/material/styles';
 import Image from "next/image";
+import Switch from '@mui/material/Switch';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 const LibroCard = ({ libro, handleReadClick, isRead }) => {
     const theme = useTheme();
 
     return (
-        <Grid item xs={12} sm={6} md={3} lg={3} sx={{padding: 1 }}>
-            <Paper elevation={2} sx={{ padding: 2, height: '100%', display: 'responsive', flexDirection: 'column', justifyContent: 'space-between' }}>
-                <div>
-                    <Typography variant="h5" gutterBottom>{libro.title}</Typography>
-                    <Image src={`/Portadaslibros/${libro.image}`} alt={libro.image} style={{ width: '100%', marginBottom: theme.spacing(2) } }  width={500} height={500} layout="responsive" />
-                    <Typography variant="body1" gutterBottom>{libro.description}</Typography>
-                    <Typography variant="body2">Q19.99</Typography>
-                    <Typography variant="body2" gutterBottom>Leído</Typography>
-                    <IconButton color="primary" aria-label="Leído" onClick={handleReadClick}>
-                        {isRead ? <CheckCircleIcon /> : <CheckBoxOutlineBlankIcon />}
-                    </IconButton>
+        <Grid item xs={12} sm={6} md={3} lg={3} sx={{ padding: 1 }}>
+            <Paper elevation={2} sx={{ padding: 1, height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', backgroundColor: '#FFE082', maxWidth: 250 }}>
+                <div style={{ textAlign: 'center' }}>
+                    <Image src={`/Portadaslibros/${libro.image}`} alt={libro.image} style={{ width: '100%', marginBottom: theme.spacing(1) }} width={200} height={300} layout="responsive" />
+                    <Typography variant="subtitle1" gutterBottom sx={{ fontWeight: 'bold' }}>{libro.title}</Typography>
                 </div>
-                <div>
-                    <Button variant="contained" color="primary" startIcon={<ThumbUpIcon />} sx={{ marginBottom: theme.spacing(1) }}>
+                <div style={{ textAlign: 'center' }}>
+                    <Button variant="contained" color="primary" startIcon={<ThumbUpIcon fontSize="small" />} sx={{ marginBottom: theme.spacing(1), fontSize: '0.8rem' }}>
                         Me gusta
                     </Button>
-                    <Button variant="contained" color="secondary" startIcon={<ThumbDownIcon />}>
+                    <Button variant="contained" color="secondary" startIcon={<ThumbDownIcon fontSize="small" />} sx={{ fontSize: '0.8rem' }}>
                         No me gusta
                     </Button>
+                </div>
+                <div style={{ textAlign: 'center' }}>
+                    <Typography variant="body2" gutterBottom>¿Ya has leído este título antes?</Typography>
+                    <FormControlLabel
+                        control={<Switch checked={isRead} onChange={handleReadClick} />}
+                        label=""
+                    />
                 </div>
             </Paper>
         </Grid>
